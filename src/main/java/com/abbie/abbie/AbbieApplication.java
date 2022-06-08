01b574659a6a952d4a75c9607711038b7bfec5e1
Abbiechebet <abigaelchebet85@gmail.com>
2022-06-08 22:24:33 +0300

    this is my first git commit

---
+++ src/main/java/com/abbie/abbie/AbbieApplication.java
@@ -0,0 +1,13 @@
+package com.abbie.abbie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AbbieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AbbieApplication.class, args);
+	}
+
+}
